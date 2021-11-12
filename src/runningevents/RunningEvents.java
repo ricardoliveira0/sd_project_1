1c1b2908245edbd09bb1dc453caafe91d91385be
Ricardo <l42647@alunos.uevora.pt>
2021-11-12 12:33:03 +0000

    update: Running Events Interface
    Add to interface all methods that will be needed

--- src/runningevents/RunningEvents.java
+++ src/runningevents/RunningEvents.java
@@ -10,5 +10,17 @@ package runningevents;
  */
 
 public interface RunningEvents extends java.rmi.Remote {
+    public void registerEvent(String name, String date);
     
+    public void getDayEvents(String date);
+    
+    public void registerParticipant(String name, String gender, String echelon);
+    
+    public void listParticipants(String name);
+    
+    public void setParticipantTrialTime(String name, int number, String time);
+    
+    public void getGeneralScoreboard(String name, int type);
+    
+    public void getPodium(String name, int echelon);
 }
